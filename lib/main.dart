06d0c6e8ef5c4e2042ff5237149fda69fca8e39c
hozdemir1968 <hozdemir1968@gmail.com>
2022-12-23 14:52:13 +0300

    20221223

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'themes/dark_theme.dart';
 import 'views/home_page.dart';
 import 'views/score_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   await EasyLocalization.ensureInitialized();
   runApp(
     EasyLocalization(
